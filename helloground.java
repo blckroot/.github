3fe0e693fcf0644f1f47afed7c6bb3d2ce4c8e66
blckroot <122182642+blckroot@users.noreply.github.com>
2023-01-07 23:14:56 +0100

    Create helloground.java

---
+++ helloground.java
@@ -0,0 +1,9 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello Ground!" to standard output.
+ */
+class HelloGrounddApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Ground!"); // Displays: Hello Grond!
+    }
+}
